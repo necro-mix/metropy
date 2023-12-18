8d1db8489b31c295353a5a082cf7f305a4843e21
BuildTools <unconfigured@null.spigotmc.org>
2023-12-17 22:44:58 -0300

    first screen

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pygame
 
-screen = pygame.init()
 
 
+pygame.init()
+
+config = {
+    "fps": 60,
+    "size": (500, 500),
+    "caption": "caption",
+    "icon": "images/d72fb4f666a872b84759bb9e636aedb1.png"
+}
+
+screen = pygame.display.set_mode(config["size"])
+pygame.display.set_caption(config["caption"])
+image = pygame.image.load(config['icon'])
+pygame.display.set_icon(image)
+clock = pygame.time.Clock()
+running = True
+while running:
+    clock.tick(config["fps"])
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.flip()
+
+
+pygame.quit()
