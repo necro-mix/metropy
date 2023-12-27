fc8506cc940a526434816fe39259f1d83ba8d043
Your Name <you@example.com>
2023-12-27 18:57:58 -0300

    added sprite, jump, background

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import pygame
 
 pygame.init()
 
-width, height = 800, 600
+WIDTH, HEIGHT = 800, 500
+GROUND_HEIGHT = 500
 
 config = {
     "ticks": 60,
-    "size": (width, height),
+    "size": (WIDTH, HEIGHT),
     "caption": "Metropy",
-    "icon": "images/d72fb4f666a872b84759bb9e636aedb1.png"
+    "icon": "img/d72fb4f666a872b84759bb9e636aedb1.png",
+    "ground_height": GROUND_HEIGHT 
 }
 
 screen = pygame.display.set_mode(config["size"])
@@ -17,62 +19,88 @@ pygame.display.set_caption(config["caption"])
 image = pygame.image.load(config['icon'])
 pygame.display.set_icon(image)
 
-ball2_radius = 20
-ball2_x = width/2
-ball2_y = height/2
-ball2_speed = 5
-# Set up ball properties
-ball1_radius = 20
-ball1_x = width // 2
-ball1_y = height // 2
-ball1_speed = 5
+# Load the background image
+background_image = pygame.image.load("img/room.jpg").convert()
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
+# Create the Mono sprite class
+class Mono(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("img/mono.png").convert_alpha()
+        # Adjust the size of the image if needed
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH // 2, config['ground_height'] - 20)
+        self.velocity = 6
+        self.jump_count = 10
+        self.on_ground = True
+
+    def update(self):
+        # Apply gravity
+        if not self.on_ground:
+            if self.rect.y < config['ground_height']- 40:
+                self.rect.y += self.velocity
+            else:
+                # Reset jump variables and position when on the ground
+                self.jump_count = 10
+                self.on_ground = True
+                self.rect.y = config['ground_height'] - 45
+
+
+# Create the sprite group
+all_sprites = pygame.sprite.Group()
+mono = Mono()
+all_sprites.add(mono)
+
+# Set up the clock
 clock = pygame.time.Clock()
-running = True
 
-while running:
+# Set up initial jump variables
+jump = False
 
-    screen.fill((255, 255, 255))
 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a]:
-        if ball2_x - ball2_radius < 0:
-            ball2_x = 0 + ball2_radius
-        ball2_x -= ball2_speed
-    if key[pygame.K_d]:
-        if ball2_x + ball2_radius > width:
-            ball2_x = width - ball2_radius
-        ball2_x += ball2_speed
-    if key[pygame.K_w]:
-        if ball2_y - ball2_radius < 0:
-            ball2_y = 0 + ball2_radius
-        ball2_y -= ball2_speed
-    if key[pygame.K_s]:
-        if ball2_y + ball2_radius > height:
-            ball2_y = height - ball2_radius
-        ball2_y += ball2_speed
-    # Move the ball
-    ball1_x += ball1_speed
 
-    # Check if the ball is out of bounds and change direction
-    if ball1_x + ball1_radius > width or ball1_x - ball1_radius < 0:
-        ball1_speed = -ball1_speed
+    keys = pygame.key.get_pressed() 
 
-    # Fill the background
-    screen.fill((255, 255, 255))
+    if keys[pygame.K_a] and mono.rect.left > 0:
+        mono.rect.x -= mono.velocity
 
-    # Draw the ball
-    pygame.draw.circle(screen, (0, 200, 50), (int(ball2_x), int(ball2_y)), ball1_radius)
-    pygame.draw.circle(screen, (0, 0, 255), (int(ball1_x), int(ball1_y)), ball1_radius)
+    if keys[pygame.K_d] and mono.rect.right < WIDTH:
+        mono.rect.x += mono.velocity
 
-    if (ball1_x, ball1_y) == (ball2_x, ball2_y):
-        sys.exit()
+    # Jump
+    if not jump and mono.on_ground:
+        if keys[pygame.K_SPACE]:
+            jump = True
+            mono.on_ground = False
+    else:
+        if mono.jump_count >= -10:
+            neg = 1
+            if mono.jump_count < 0:
+                neg = -1
+            mono.rect.y -= (mono.jump_count ** 2) * 0.5 * neg
+            mono.jump_count -= 1
+        else:
+            jump = False
+            mono.jump_count = 10
 
+    # Draw the background
+    screen.blit(background_image, (0, 0))
+
+    # Draw the sprites
+    all_sprites.draw(screen)
+
+    # Update the display
     pygame.display.flip()
-    clock.tick(config["ticks"])
 
-pygame.quit()
+    # Update the sprite group
+    all_sprites.update()
+
+    # Cap the frame rate
+    clock.tick(config['ticks'])
